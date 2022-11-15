58ee984532dc5937144b5da8dfc1a00c03b6d7ea
taylorJ <taylor.ji719@gmail.com>
2022-11-14 19:06:09 -0800

    update cannonball's move method, added outofBound method for checking if the ball is inside the window, if the ball goes outside then bounce it

--- src/main/java/CannonBall.java
+++ src/main/java/CannonBall.java
@@ -19,11 +19,10 @@ public class CannonBall {
   int fillColour = 255;
 
 
-  public CannonBall(float xin, float yin, Window window) {
-    this.xpos = xin;
-    this.ypos = yin;
+  public CannonBall(PVector position, PVector direction, Window window) {
+    this.position = position;
+    this.direction = direction;
     this.window = window;
-
   }
 
 
@@ -33,17 +32,35 @@ public class CannonBall {
     return false;
   }
 
+
+  public boolean outOfBounds(Window window) {
+    if ((this.position.x > window.width
+            || this.position.x < 0)
+            || (this.position.y > window.height
+            || this.position.y < 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void bounce(float b) {
+    this.direction.rotate(b);
+  }
   public void move(Window window) {
     //this method will be implemented from IMovable interface
     // cannonball moves toward in certain direction
     this.position = this.position.add(this.direction.mult(speed));
+    if (outOfBounds(window)) {
+      bounce((float) Math.PI / 4f);
+    }
 
   }
 
   public void draw(Window window) {
     window.fill(this.fillColour);
-    window.ellipse(this.xpos,
-            this.ypos,
+    window.ellipse(this.position.x,
+            this.position.y,
             this.width,
             this.height);
   }
