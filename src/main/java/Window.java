8230563b38c038c399391a0a4b91cdfc3019d472
Nash Baek <nash4comp@gmail.com>
2022-11-15 18:19:35 -0800

    Adjust angle

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -15,6 +15,7 @@ public class Window extends PApplet{
   protected PVector tempDir = new PVector(1f, 1f).normalize();
 
   protected CannonBall ball = new CannonBall(tempPos, tempDir,this);
+  protected PVector angleDirection = new PVector(3,1).normalize();
   protected int width = 1280;
   protected int height = 720;
 
@@ -46,7 +47,7 @@ public class Window extends PApplet{
     rightPlayer.draw(this);
     ball.draw(currentPlayer.position, this);
     ball.move(this);
-
+    drawAngle(currentPlayer, angleDirection);
     drawDashboard();
     drawHp();
     drawFuel();
@@ -97,6 +98,34 @@ public class Window extends PApplet{
     this.rect(this.width - 240, this.height - 40, rightPlayer.getFuel(), 10);
   }
 
+  public void drawAngle(Player player, PVector angleDirection) {
+    float xPos;
+    float yPos;
+    xPos = player.getPosition().x;
+    yPos = player.getPosition().y;
+    this.stroke(255,179,179);
+    this.line(xPos + player.width, yPos, xPos + (angleDirection.x) * 50, yPos - (angleDirection.y) * 50);
+    this.stroke(0);
+  }
+
+  public void setAngleDirection(double degree) {
+    PVector direction = new PVector(this.angleDirection.x,
+                      this.angleDirection.y);
+//    PVector direction = new PVector(this.angleDirection.x + (float)degree,
+//            this.angleDirection.y - (float)degree).normalize();
+    if ((getAngle(this.angleDirection.x + degree, this.angleDirection.y - degree) > 0.9)
+      || (getAngle(this.angleDirection.x + degree, this.angleDirection.y - degree) < 0)) {
+      return;
+    } else {
+      this.angleDirection.x += degree;
+      this.angleDirection.y -= degree;
+    }
+  }
+
+  double getAngle(double x, double y) {
+    return atan((float)y/(float)x);
+  }
+
   public void settings() {
     size(this.width, this.height);
     img = loadImage("title.jpg");
@@ -122,6 +151,12 @@ public class Window extends PApplet{
       case LEFT:
         currentPlayer.move(-10);
         currentPlayer.setFuel(10);
+        break;
+      case UP:
+        setAngleDirection(-0.01);
+        break;
+      case DOWN:
+        setAngleDirection(0.01);
         break;
       default:
         break;
